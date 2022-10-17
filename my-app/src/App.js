92cf2e2abc06db42a8e412b6dd3af53e59b657be
Samara Silvia <100232025+SamaraSilvia81@users.noreply.github.com>
2022-10-17 18:05:14 -0300

    Add files via upload

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,9 +2,10 @@ import HelloWorld from './components/HelloWorld'
 import SayMyName from './components/SayMyName'
 import Pessoa from './components/Pessoa'
 import './App.css';
+import List from './components/List';
 
 function App() {
-
+ 
   const nome = "Joshua"
 
   return (
@@ -18,10 +19,13 @@ function App() {
         </div>
       <div>
         <HelloWorld/>
-        <SayMyName nome="Wanda"/>
+        <SayMyName nome = 'Wanda'/>
         <SayMyName nome={nome}/> 
+        <SayMyName nome="Samara"/>
       </div>
-    
+      <div>
+        <List/>
+      </div>
     </div>
   );
 }
